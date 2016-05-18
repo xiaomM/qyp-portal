048562bc6d7d6234a1216b169ad037b0f1a72ace
二凢 <dingyu.wdy@alibaba-inc.com>
2016-05-18 15:37:00 +0800

    add

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ var config = {
 
     // 后端域名
     "backendHost": 'http://127.0.0.1:8091',
-    "staticHost": 'http://www.dream623.com',
+    "staticHost": 'http://g.tbcdn.cn/platform/static',
 
 };
 
