f7f2b6b148ebe78ac35ac09972304aae3d61f5b8
zhangxiaoming <zhangxiaoming@51xianqu.net>
2016-07-18 17:49:50 +0800

    $.fn.serializeObject = function() {     var o = {};     var a = this.serializeArray();     $.each(a, function() {         if (o[this.name] !== undefined) {             if (!o[this.name].push) {                 o[this.name] = [o[this.name]];             }             o[this.name].push(this.value || '');         } else {             o[this.name] = this.value || '';         }     });     return o; };

--- public/js/my-weui.js
+++ public/js/my-weui.js
@@ -1,5 +1,22 @@
 var weuiQyp = {};
 
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 $(document)
     .on('click', '#J_sign_up_activity', function (e) {
         e.preventDefault();
