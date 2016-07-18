6ad04d8b2ad45c891ec5b89d4ac7d26e36b2d855
zhangxiaoming <zhangxiaoming@51xianqu.net>
2016-07-18 15:27:42 +0800

    my-weui.js

---
+++ public/js/my-weui.js
@@ -0,0 +1,28 @@
+var weuiQyp = {};
+
+$(document)
+    .on('click', '#J_sign_up_activity', function (e) {
+        e.preventDefault();
+        var $target = $(this);
+        var $form = $target.closest('form');
+
+        $form.validate();
+
+        if (!$form.valid()) {
+            return false;
+        }
+
+        var params = $form.serializeObject();
+        $.ajax({
+            url: '/ajax/activity/signup',
+            type: 'POST',
+            data: params,
+            success: function (result) {
+                if (result.success) {
+                    location.href = '/activity/success/'+result.data._id
+                } else {
+                    $.toast(result.errorMsg || '报名失败',"forbidden")
+                }
+            }
+        });
+    })
