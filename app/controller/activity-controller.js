a0db227da612cbc00bd7d20f4a9a9fe892339f17
zhangxiaoming <zhangxiaoming@51xianqu.net>
2016-07-19 03:06:57 +0800

    error

--- app/controller/activity-controller.js
+++ app/controller/activity-controller.js
@@ -30,6 +30,17 @@ exports.getDetail = function* (next) {
 
     var activityEntity = yield ActivityModel.getActivityByActivityId(activityId);
     //console.log(data)
+    var dutyList = activityEntity.dutyList.toString();
+    if (dutyList) {
+        console.log('dutyList='+activityEntity.dutyList.split('|'));
+        activityEntity.dutyList = activityEntity.dutyList.split('|');
+        console.log('dutyList='+activityEntity.dutyList);
+    }
+    var boardList = activityEntity.boardList.toString();
+    if (boardList) {
+        activityEntity.boardList = activityEntity.boardList.split('|');
+        console.log('boardList='+activityEntity.boardList);
+    }
     ctx.locals.activityDetail = activityEntity;
     yield next
 };
