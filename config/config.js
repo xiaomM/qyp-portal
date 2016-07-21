16288b5b04de15266f2e07e7629316e122b82914
zhangxiaoming <zhangxiaoming@51xianqu.net>
2016-07-21 21:59:45 +0800

    http://www.dream623.com/activity/jspay/notify_url'

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ var config = {
     "env":"production",
     "appName": "book",
     //端口号配置
-    "port": 8080,
+    "port": 8088,
     //模板所在的目录
     "viewDir": path.join(__dirname,'..','app/view'),
     //log所在的目录
