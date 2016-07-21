3dccd805763fab66405541251ed7d599c830f224
zhangxiaoming <zhangxiaoming@51xianqu.net>
2016-07-21 22:33:31 +0800

    http://www.dream623.com/activity/jspay/notify_url'

--- config/local.js
+++ config/local.js
@@ -8,4 +8,10 @@ module.exports = {
     db:{
         uri: 'mongodb://127.0.0.1:27017/qyp-dev'
     },
+    wepay:{
+        appid: 'wx29f2877576637978',
+        mch_id: '1357284002',
+        partner_key: '5bbe06ef351b78a89223588665682435', //微信商户平台API密钥
+        pfxPath: '/qyp/keys/apiclient_cert.p12' //微信商户平台证书
+    }
 };
