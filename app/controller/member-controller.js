2c9f3b5059524eab1e2c652524360a1593bb5c54
zhangxiaoming <zhangxiaoming@51xianqu.net>
2016-07-27 00:22:07 +0800

    使用js移除

--- app/controller/member-controller.js
+++ app/controller/member-controller.js
@@ -9,40 +9,45 @@ let wepay = require('../model/wepay');
 
 exports.oAuth2 = function* (next) {
     let ctx = this;
-            let code = ctx.request.query.code;
-            if(code == undefined){
-                console.log(ctx.request);
-                let prefix = '';
-                if(ctx.config.env === "dev"){
-                    prefix = '/development';
-                }
-                const redirect_uri = url.format({
-                    protocol: 'http',
-                    host: 'www.dream623.com',
-                    pathname: prefix+ctx.request.url,
-                    search: ''
-                });
-
-                let urlTemplate = 'https://open.weixin.qq.com/connect/oauth2/authorize?' +
-                    'appid='+ctx.config.wepay.appid+'&redirect_uri='+redirect_uri+'&response_type=code&' +
-                    'scope=snsapi_userinfo&state=1#wechat_redirect';
-                console.log('redirectTo:'+urlTemplate);
-                ctx.response.redirect(urlTemplate);
-                return;
-            }else {
-                console.log('code=' + code);
-                let tokenResult = yield wepay.getTokenByCode(code);
-                console.log('result = ' + JSON.stringify(tokenResult));
-                let userInfo = yield wepay.getUserInfo(tokenResult.access_token, tokenResult.openid);
-                let dbUserInfo = yield MemberModel.getMember(userInfo.openid);
-                if (dbUserInfo == undefined) {
-                    yield MemberModel.saveMember(new MemberModel(userInfo));
-                }else{
-                    console.log("user had exists userinfo="+JSON.stringify(dbUserInfo));
-                }
-                console.log('userInfo='+JSON.stringify(userInfo));
-                ctx.locals.userInfo = userInfo;
+    if(ctx.session.userInfo == undefined) {
+        let code = ctx.request.query.code;
+        if (code == undefined) {
+            console.log(ctx.request);
+            let prefix = '';
+            if (ctx.config.env === "dev") {
+                prefix = '/development';
             }
+            const redirect_uri = url.format({
+                protocol: 'http',
+                host: 'www.dream623.com',
+                pathname: prefix + ctx.request.url,
+                search: ''
+            });
+
+            let urlTemplate = 'https://open.weixin.qq.com/connect/oauth2/authorize?' +
+                'appid=' + ctx.config.wepay.appid + '&redirect_uri=' + redirect_uri + '&response_type=code&' +
+                'scope=snsapi_userinfo&state=1#wechat_redirect';
+            console.log('redirectTo:' + urlTemplate);
+            ctx.response.redirect(urlTemplate);
+            return;
+        } else {
+            console.log('code=' + code);
+            let tokenResult = yield wepay.getTokenByCode(code);
+            console.log('result = ' + JSON.stringify(tokenResult));
+            let userInfo = yield wepay.getUserInfo(tokenResult.access_token, tokenResult.openid);
+            let dbUserInfo = yield MemberModel.getMember(userInfo.openid);
+            if (dbUserInfo == undefined) {
+                yield MemberModel.saveMember(new MemberModel(userInfo));
+            } else {
+                console.log("user had exists userinfo=" + JSON.stringify(dbUserInfo));
+            }
+            console.log('userInfo=' + JSON.stringify(userInfo));
+            ctx.locals.userInfo = userInfo;
+            ctx.session.userInfo = userInfo;
+        }
+    }else{
+        ctx.locals.userInfo = ctx.session.userInfo;
+    }
     yield next;
 }
 
